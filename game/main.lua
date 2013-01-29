55a81de2338d6ce2c4be0ea94c4a007744b926d9
Tyler Neylon <tylerneylon@gmail.com>
2013-01-29 00:50:57 -0800

    Make infinte generated world.

--- game/main.lua
+++ game/main.lua
@@ -1,4 +1,31 @@
 -- Three-space indents?!? wut. this is from a tutorial.
+
+function int_to_nonneg_int(i)
+  if i >= 0 then
+    return i * 2
+  else
+    return i * -2 - 1
+  end
+end
+
+-- Input is expected to be two nonnegative integers.
+function pair_to_pos_int(x, y)
+  diag = x + y
+  max = (diag + 1) * (diag + 2) / 2
+  return max - x
+end
+
+function rand_next(i)
+  -- I just made this up. I have no idea if it's any good.
+  return (i * 23947 + 28348) % 65535
+end
+
+function map(x, y)
+  v = pair_to_pos_int(x, y)
+  --print('from '..x..','..y..' got v='..v)
+  return rand_next(rand_next(v)) % 2
+end
+
 function love.load()
 
   -- our tiles
@@ -21,29 +48,6 @@ function love.load()
   tile_w = 48
   tile_h = 48
 
-  map={
-   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-   { 0, 1, 0, 0, 2, 2, 2, 0, 3, 0, 3, 0, 1, 1, 1, 0, 0, 0, 0, 0},
-   { 0, 1, 0, 0, 2, 0, 2, 0, 3, 0, 3, 0, 1, 0, 0, 0, 0, 0, 0, 0},
-   { 0, 1, 1, 0, 2, 2, 2, 0, 0, 3, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0},
-   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
-   { 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0},
-   { 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-   { 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-   { 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-   { 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-   { 0, 2, 2, 2, 0, 3, 3, 3, 0, 1, 1, 1, 0, 2, 0, 0, 0, 0, 0, 0},
-   { 0, 2, 0, 0, 0, 3, 0, 3, 0, 1, 0, 1, 0, 2, 0, 0, 0, 0, 0, 0},
-   { 0, 2, 0, 0, 0, 3, 0, 3, 0, 1, 0, 1, 0, 2, 0, 0, 0, 0, 0, 0},
-   { 0, 2, 2, 2, 0, 3, 3, 3, 0, 1, 1, 1, 0, 2, 2, 2, 0, 0, 0, 0},
-   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-  }
- 
   print("hi command line")
   for key, value in pairs(_G) do
     print(key, value)
@@ -58,18 +62,20 @@ end
 function love.keypressed(key, unicode)
    if key == 'up' then
       map_y = map_y-1
-      if map_y < 0 then map_y = 0; end
+      --if map_y < 0 then map_y = 0; end
    end
    if key == 'down' then
       map_y = map_y+1
-      if map_y > map_h-map_display_h then map_y = map_h-map_display_h; end
+      --if map_y > map_h-map_display_h then map_y = map_h-map_display_h; end
    end
    
    if key == 'left' then
-      map_x = math.max(map_x-1, 0)
+      map_x = map_x - 1
+      --map_x = math.max(map_x-1, 0)
    end
    if key == 'right' then
-      map_x = math.min(map_x+1, map_w-map_display_w)
+      map_x = map_x + 1
+      --map_x = math.min(map_x+1, map_w-map_display_w)
    end
 end
 
@@ -77,7 +83,7 @@ function draw_map()
    for y=1, map_display_h do
       for x=1, map_display_w do
          love.graphics.draw( 
-            tile[map[y+map_y][x+map_x] % 2], 
+            tile[map(x + map_x, y + map_y)], 
             (x*tile_w)+map_offset_x, 
             (y*tile_h)+map_offset_y )
       end
