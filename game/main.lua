df0117f7fe92c1de63afb7834d2cddda6e57074f
Tyler Neylon <tylerneylon@gmail.com>
2013-01-31 00:10:02 -0800

    Formatting cleanup.

--- game/main.lua
+++ game/main.lua
@@ -1,5 +1,3 @@
--- Three-space indents?!? wut. this is from a tutorial.
-
 function int_to_nonneg_int(i)
   if i >= 0 then
     return i * 2
@@ -22,7 +20,6 @@ end
 
 function map(x, y)
   v = pair_to_pos_int(x, y)
-  --print('from '..x..','..y..' got v='..v)
   return rand_next(rand_next(v)) % 2
 end
 
@@ -31,7 +28,7 @@ function love.load()
   -- our tiles
   tile = {}
   for i = 0, 1 do
-     tile[i] = love.graphics.newImage( "img/tile"..i..".png" )
+    tile[i] = love.graphics.newImage( "img/tile"..i..".png" )
   end
 
   hero = {}
@@ -66,41 +63,41 @@ function love.draw()
 end
 
 function love.keypressed(key, unicode)
-   if key == 'up' then
-      map_y = map_y-1
-      --if map_y < 0 then map_y = 0; end
-   end
-   if key == 'down' then
-      map_y = map_y+1
-      --if map_y > map_h-map_display_h then map_y = map_h-map_display_h; end
-   end
+  if key == 'up' then
+    map_y = map_y-1
+    --if map_y < 0 then map_y = 0; end
+  end
+  if key == 'down' then
+    map_y = map_y+1
+    --if map_y > map_h-map_display_h then map_y = map_h-map_display_h; end
+  end
    
-   if key == 'left' then
-      map_x = map_x - 1
-      --map_x = math.max(map_x-1, 0)
-   end
-   if key == 'right' then
-      map_x = map_x + 1
-      --map_x = math.min(map_x+1, map_w-map_display_w)
-   end
+  if key == 'left' then
+    map_x = map_x - 1
+    --map_x = math.max(map_x-1, 0)
+  end
+  if key == 'right' then
+    map_x = map_x + 1
+    --map_x = math.min(map_x+1, map_w-map_display_w)
+  end
 
-   clock = clock + 1
+  clock = clock + 1
 end
 
 function draw_map()
-   for y=1, map_display_h do
-      for x=1, map_display_w do
-         love.graphics.draw( 
-            tile[map(x + map_x, y + map_y)], 
-            (x*tile_w)+map_offset_x, 
-            (y*tile_h)+map_offset_y )
-      end
-   end
+  for y = 1, map_display_h do
+    for x = 1, map_display_w do
+      love.graphics.draw( 
+          tile[map(x + map_x, y + map_y)], 
+          (x * tile_w) + map_offset_x, 
+          (y * tile_h) + map_offset_y)
+    end
+  end
 
-   mid_x, mid_y = map_display_w / 2, map_display_h / 2
-   love.graphics.draw(
-       hero[clock % 2],
-       mid_x * tile_w + map_offset_x,
-       mid_y * tile_h + map_offset_y)
-
+  mid_x, mid_y = map_display_w / 2, map_display_h / 2
+  love.graphics.draw(
+      hero[clock % 2],
+      mid_x * tile_w + map_offset_x,
+      mid_y * tile_h + map_offset_y)
 end
+
